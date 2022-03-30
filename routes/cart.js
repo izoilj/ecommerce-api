3d08482a43d83203430f41c722ea3ff773990a99
izoilj <ljy9407@gmail.com>
2022-03-30 02:28:24 -0600

    Made Order parts

--- routes/cart.js
+++ routes/cart.js
@@ -48,7 +48,7 @@ router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
 //GET USER CART - everybody can see Carts
 router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const Cart = await Cart.find({ userId: req.params.userId });
+    const cart = await Cart.find({ userId: req.params.userId });
     res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);
