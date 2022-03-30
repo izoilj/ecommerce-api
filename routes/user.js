b95866795fdc5088bf0e2f225353dd8d73b384f4
izoilj <ljy9407@gmail.com>
2022-03-29 23:15:39 -0600

    Built a user auth for register and login using crypto ans json web token

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,3 @@
 const router = require('express').Router();
 
-// module.exports = router;
-
-// That for just test
-// router.get('/usertest', (req, res) => {
-//   res.send('user test is successfull');
-// });
-
-// router.post('/userposttest', (req, res) => {
-//   const username = req.body.username;
-//   console.log(username); // node에 보임
-//   res.send('your username is: ' + username); // 실제 보여지는 값
-// });
-
 module.exports = router;
